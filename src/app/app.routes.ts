59913b4f6b27229647ae91c60c0d9a210d0f42c6
Emmanuella Batubo <emmanuellabatubo@icloud.com>
2025-10-22 23:09:39 +0100

    chore(init): initialize Angular app and setup base structure
    - generated Angular app with SCSS and routing
    - added Angular Material and default theme
    - created shared, core, and features folders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SignUp } from './features/sign-up/sign-up/sign-up';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: SignUp,
+      },
+];
