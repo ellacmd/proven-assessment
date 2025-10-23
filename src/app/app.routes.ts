fe4cdac526957a2ae3cfc6a52c6bd9cb9562c190
Emmanuella Batubo <emmanuellabatubo@icloud.com>
2025-10-23 20:01:10 +0100

    feat(UI): built profile page# <type>(<scope>): <short summary>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { SignUp } from './features/sign-up/sign-up/sign-up';
+import { ProfilePage } from './features/profile-page/profile-page/profile-page';
 
 export const routes: Routes = [
     {
         path: '',
         component: SignUp,
+      },
+      {
+        path: 'profile',
+        component: ProfilePage,
       },
 ];
